b33f43ad9500e7659a5743462ae1270e66cf80d9
ol100 <javiercalle3@gmail.com>
2021-07-03 15:01:55 +0200

    Faltaba esto, usado para la memoria del TFG

--- memoria.h
+++ memoria.h
@@ -8,10 +8,17 @@ extern unsigned char cartucho[32768];
 extern unsigned char switchRAM[8192];
 extern unsigned char HRAM[128];
 
+extern unsigned char ff04;
+extern unsigned char ff05;
+extern unsigned char ff06;
+extern unsigned char ff07;
 
+void DMATransfer(unsigned char a);
 void writeMEMB(unsigned short dir, unsigned char a);
 void writeMEM16(unsigned short dir, unsigned short a);
 unsigned char loadMEMB(unsigned short dir);
 unsigned short loadMEM16(unsigned short dir);
 unsigned short loadMEM16pila(unsigned short *puntero);
 void writeMEM16pila(unsigned short valor, unsigned short *puntero);
+unsigned char lecturadirecta(unsigned short dir);
+void escrituradirecta(unsigned short dir, unsigned char a);
